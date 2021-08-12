8b90ab7eb14d0e445f23aed213bb60c35e23fd65
Rguardado1009 <rguardado1009@gmail.com>
2021-08-12 11:52:36 -0400

    commit tree 2c74c679b73c6a4ce5801d3bb433e6208db56ba7

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,14 @@
 // Code EyesOnMe Component Here
+function btn() {
+	return (
+		<>
+			<button
+				onFocus={() => console.log("Good!")}
+				onBlur={() => console.log("Hey! Eyes on me!")}
+			>
+				"Eyes on me!"
+			</button>
+		</>
+	);
+}
+export default btn;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,14 @@
 // Code Keypad Component Here
+
+function Keypad() {
+	function change(e) {
+		console.log(`${e.target.name}: ${e.target.value}`);
+	}
+	return (
+		<>
+			<input type="password" name="Entering Password..." onChange={change} />
+		</>
+	);
+}
+
+export default Keypad;
